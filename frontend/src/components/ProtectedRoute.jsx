e639cc16ac0023f27cfceb67105f7385a3f939b0
wee08 <227932670+seanmanutnithya@users.noreply.github.com>
2026-09-17 14:20:59 +0700

    rename folder

--- frontend/src/components/ProtectedRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate } from "react-router-dom";
-import { useAuther } from "@/context/AuthContext";
-
-const ProtectedRoute = ({ children }) => {
-  const { isAuthericated } = useAuther();
-  return isAuthericated ? children : <Navigate to="/login" replace />;
-};
-
-export default ProtectedRoute;
